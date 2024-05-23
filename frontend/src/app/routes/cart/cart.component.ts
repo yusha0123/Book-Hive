899e8dafd7b9d142cf37baf47e2e5be7f67c6990
Yusha Tahlil <ymmttmy5@gmail.com>
2024-05-23 16:40:58 +0530

    Cart page complete

--- frontend/src/app/routes/cart/cart.component.ts
+++ frontend/src/app/routes/cart/cart.component.ts
@@ -11,12 +11,36 @@ export class CartComponent implements OnInit {
   constructor(private cartService: CartService) {}
 
   cartItems: CartItem[] = [];
+  totalPrice: number = 0;
 
   ngOnInit(): void {
     this.loadCartItems();
   }
 
+  calculateTotalPrice(): void {
+    this.totalPrice = this.cartItems.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    );
+  }
+
   loadCartItems(): void {
     this.cartItems = this.cartService.cartItems;
+    this.calculateTotalPrice();
+  }
+
+  incrementItem(itemId: string): void {
+    this.cartService.incrementCartItem(itemId);
+    this.loadCartItems();
+  }
+
+  decrementItem(itemId: string): void {
+    this.cartService.decrementCartItem(itemId);
+    this.loadCartItems();
+  }
+
+  removeItem(itemId: string): void {
+    this.cartService.removeFromCart(itemId);
+    this.loadCartItems();
   }
 }
